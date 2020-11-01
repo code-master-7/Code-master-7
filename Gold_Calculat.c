256d6a09af8c5d06c70d0894d6fba411f929fad3
code-master-7 <71135732+code-master-7@users.noreply.github.com>
2020-11-01 17:12:04 +0530

    Update Gold_Calculat.c

--- Gold_Calculat.c
+++ Gold_Calculat.c
@@ -20,30 +20,7 @@ int main(void)
     printf("\n\n");
 
     printf("Gold price = %.4f \nWait = %.4f \nlabor = %.4f \nYour price shoud be = %.4f \n" , gp , w , l , s3);
-
-}
-
-/*#include <stdio.h>
-
-int main()
-{
-    float Gold_price, Wait, Sum1, Sum2, Sum3, Labor;
-
-    printf("Enter an gold price\n");
-    scanf("%f\n", &Gold_price);
-
-    printf("Enter the wait of Jewellery \n");
-    scanf("%f\n", &Wait);
-
-    printf("Enter labor cost on jewellery\n");
-    scanf("%f\n", &Labor);
-
-    Sum1 = Gold_price * Wait; // Sum 1 = Gold_price * Wait
-    Sum2 = Labor * Wait;      // Sum 2 = Labor * Wait
-    Sum3 = Sum1 + Sum2;       // Sum3 = Sum1 + Sum2
-
-    printf("In Gold price = %.4f \nwith Wait = %.4f \nAnd Labor = %.4f \nPrice = %.4f", Gold_price, Wait, Labor, Sum3);
-
+    
     return 0;
+
 }
-*/
